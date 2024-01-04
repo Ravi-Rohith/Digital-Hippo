47c26c0cfe51d58762f17a7e44d734360bba5e88
Ravi-Rohith <90379626+Ravi-Rohith@users.noreply.github.com>
2024-01-04 22:34:29 +0530

    started hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-        <h1>Your Marketplace for high-quality <span className="text-violet-600">digital assets</span></h1>
+        <h1 className="font-bold tracking-tight text-4xl text-gray-900 sm:text-6xl">Your Marketplace for high-quality <span className="text-violet-600">digital assets</span>.</h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">Welcome to DigitalHippo. Every asset on our platform is verified by our team to ensure the highest quality standards.</p>
       </div>
     </MaxWidthWrapper>
   )
