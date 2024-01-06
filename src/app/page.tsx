ba005b23088d263a5810238b8fbdbd66a81805f0
Ravi-Rohith <90379626+Ravi-Rohith@users.noreply.github.com>
2024-01-06 20:13:56 +0530

    hero in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,6 +8,9 @@ export default function Home() {
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
         <h1 className="font-bold tracking-tight text-4xl text-gray-900 sm:text-6xl">Your Marketplace for high-quality <span className="text-violet-600">digital assets</span>.</h1>
         <p className="mt-6 text-lg max-w-prose text-muted-foreground">Welcome to DigitalHippo. Every asset on our platform is verified by our team to ensure the highest quality standards.</p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href="/products" className={buttonVariants()}>Browse trending</Link>
+        </div>
       </div>
     </MaxWidthWrapper>
   )
